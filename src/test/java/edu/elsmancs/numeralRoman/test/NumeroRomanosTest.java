325cce559a8d891ad803c17e8a88d2c7ac27cddc
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-09-21 19:18:15 +0200

    feat(NumeroRomanosTest): Añadir test NumeroRomanosTest

--- src/test/java/edu/elsmancs/numeralRoman/test/NumeroRomanosTest.java
+++ src/test/java/edu/elsmancs/numeralRoman/test/NumeroRomanosTest.java
@@ -1,4 +1,9 @@
 package edu.elsmancs.numeralRoman.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 public class NumeroRomanosTest {
+
 }
