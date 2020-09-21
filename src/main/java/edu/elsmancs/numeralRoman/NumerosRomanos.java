7a541c0b6d93097639838028dce410066a51a446
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-09-21 19:15:48 +0200

    feat(NumerosRomanos): Añadir clase NumerosRomanos

---
+++ src/main/java/edu/elsmancs/numeralRoman/NumerosRomanos.java
@@ -0,0 +1,24 @@
+package edu.elsmancs.numeralRoman;
+
+public class NumerosRomanos {
+
+    private String numeroLetra = null;
+    private int numeroDecimal = 0;
+
+
+    public String getNumeroLetra() {
+        return numeroLetra;
+    }
+
+    public void setNumeroLetra(String numeroLetra) {
+        this.numeroLetra = numeroLetra;
+    }
+
+    public int getNumeroDecimal() {
+        return numeroDecimal;
+    }
+
+    public void setNumeroDecimal(int numeroDecimal) {
+        this.numeroDecimal = numeroDecimal;
+    }
+}
