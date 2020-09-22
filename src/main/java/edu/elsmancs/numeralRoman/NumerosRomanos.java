9dd847771f8db7c073f01932b2a859b52a160e7b
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-09-22 14:39:07 +0200

    refactor(NumerosRomanos): Refactorizar expresión regular diccionario

--- src/main/java/edu/elsmancs/numeralRoman/NumerosRomanos.java
+++ src/main/java/edu/elsmancs/numeralRoman/NumerosRomanos.java
@@ -1,11 +1,17 @@
 package edu.elsmancs.numeralRoman;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NumerosRomanos {
 
     private String numeroLetra = null;
-    private int numeroDecimal = 0;
+    private short numeroDecimal = 0;
 
-    private RegexNumerosRomanos regexDiccionario = new RegexNumerosRomanos();
+    private RegexNumerosRomanos regexMapa = new RegexNumerosRomanos();
+
     public NumerosRomanos() {
     };
 
@@ -19,17 +25,61 @@ public class NumerosRomanos {
 
     public void setNumeroLetra(String numeroLetra) {
         this.numeroLetra = numeroLetra;
+        this.setNumeroDecimal((short) 0);
     }
 
-    public int getNumeroDecimal() {
+    public short getNumeroDecimal() {
         return numeroDecimal;
     }
 
-    public void setNumeroDecimal(int numeroDecimal) {
+    public void setNumeroDecimal(short numeroDecimal) {
         this.numeroDecimal = numeroDecimal;
     }
 
-    public void setRegexDiccionario(RegexNumerosRomanos regex) {
-        this.regexDiccionario = regex;
+    public void setRegexMapa(RegexNumerosRomanos regex) {
+        this.regexMapa = regex;
+    }
+
+    public RegexNumerosRomanos getRegexMapa() {
+        return this.regexMapa;
+    }
+
+    public void initRegexDicionario() {
+        getRegexMapa().addRegex("grupoSumatorio", "(?<!C)[DM]|(?<!X)[LC](?![DM])|(?<!I)[VX](?![LC])|I(?![VX])");
+        getRegexMapa().addRegex("grupoSustractivo", "(C[DM])|(X[LC])|(I[VX])");
+    }
+
+    public void addRegex(String descripcion, String regex) {
+        getRegexMapa().addRegex(descripcion, regex);
+    }
+
+    public List<String> getExpresionesRegulares() {
+        List<String> listaRegex = new ArrayList<String>(getRegexMapa().getRegex().values());
+        return listaRegex;
+    }
+
+    public short toDecimal() {
+        for(String regex : getRegexMapa().getValues()) {
+            Matcher matcher = createMatcher(regex);
+            groupSumatoryToDecimal(matcher);
+        }
+        return getNumeroDecimal();
+    }
+
+    private Matcher createMatcher(String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(this.numeroLetra);
+        return matcher;
+    }
+
+    private void groupSumatoryToDecimal(Matcher matcher) {
+        while (matcher.find()) {
+            this.numeroDecimal += valorDecimal(matcher.group());
+        }
+    }
+
+    public short valorDecimal(String numeroRomano) {
+        SimbolosRomanos simbolo = Enum.valueOf(SimbolosRomanos.class, String.valueOf(numeroRomano));
+        return (short) simbolo.getValorDecimal();
     }
 }
